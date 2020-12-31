51e0470dd9c2cf539947211c3a3cbc632f076e6d
ninahorne <nina.horne1221@gmail.com>
2020-12-30 19:49:08 -0600

    videos are closing

--- scripts.js
+++ scripts.js
@@ -2,29 +2,39 @@
 // nav bar open functions
 
 function menuToggle() {
-  var menuToggle = document.getElementById("menuToggle");
-  var mainMenu = document.getElementById("mainMenu");
-  var overlay = document.querySelector(".menu-overlay");
+  var menuToggle = document.getElementById('menuToggle');
+  var mainMenu = document.getElementById('mainMenu');
+  var overlay = document.querySelector('.menu-overlay');
 
-  menuToggle.classList.toggle("active");
-  mainMenu.classList.toggle("closed");
-  overlay.classList.toggle("closed");
+  menuToggle.classList.toggle('active');
+  mainMenu.classList.toggle('closed');
+  overlay.classList.toggle('closed');
 }
 
 //PARALLAX FUNCTION
-document.addEventListener("scroll", () => {
+document.addEventListener('scroll', () => {
   var top = window.pageYOffset;
-  var one = document.querySelector(".one");
-  var two = document.querySelector(".two");
-  var three = document.querySelector(".three");
-  var four = document.querySelector(".four");
-  var five = document.querySelector(".five");
+  var one = document.querySelector('.one');
+  var two = document.querySelector('.two');
+  var three = document.querySelector('.three');
+  var four = document.querySelector('.four');
+  var five = document.querySelector('.five');
 
-  one.style.bottom = -(top * 0.15) + "px";
-  two.style.bottom = -(top * 0.2) + "px";
-  three.style.bottom = -(top * 0.3) + "px";
-  four.style.bottom = -(top * 0.4) + "px";
-  five.style.bottom = -(top * 0.5) + "px";
+  if (one) {
+    one.style.bottom = -(top * 0.15) + 'px';
+  }
+  if (two) {
+    two.style.bottom = -(top * 0.2) + 'px';
+  }
+  if (three) {
+    three.style.bottom = -(top * 0.3) + 'px';
+  }
+  if (four) {
+    four.style.bottom = -(top * 0.4) + 'px';
+  }
+  if (five) {
+    five.style.bottom = -(top * 0.5) + 'px';
+  }
 });
 
 // // background height setter
@@ -41,63 +51,62 @@ document.addEventListener("scroll", () => {
 
 // window.addEventListener('resize', setBackgroundHeight());
 
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener('DOMContentLoaded', function () {
   setTimeout(() => {
-    var mountainQuote = document.getElementById("mountain-quote");
+    var mountainQuote = document.getElementById('mountain-quote');
     if (mountainQuote) {
-      mountainQuote.classList.add("zoom");
+      mountainQuote.classList.add('zoom');
     }
   }, 200);
 
-  var modals = document.getElementsByClassName("modal");
+  var modals = document.getElementsByClassName('modal');
   if (modals) {
     var windowWidth = window.innerWidth;
     for (let i = 0; i < modals.length; i++) {
-      modals[i].style.maxWidth = windowWidth + "px";
+      modals[i].style.maxWidth = windowWidth + 'px';
     }
   }
 
-  var mountains = document.querySelector(".mountains");
-  var mountainWrapper = document.querySelector(".mountain-wrapper");
+  var mountains = document.querySelector('.mountains');
+  var mountainWrapper = document.querySelector('.mountain-wrapper');
 
-  var mountainQuote = document.querySelector(".mountain-quote");
-  var siteTitle = document.querySelector(".site-title");
-  var arrow = document.querySelector(".arrow");
-  var scrollDown = document.querySelector(".scroll_down");
+  var mountainQuote = document.querySelector('.mountain-quote');
+  var siteTitle = document.querySelector('.site-title');
+  var arrow = document.querySelector('.arrow');
+  var scrollDown = document.querySelector('.scroll_down');
   let screenWidth = window.innerWidth;
   let screenHeight = window.innerHeight;
   let ratio = screenWidth / screenHeight;
   if (ratio < 0.8 && screenWidth > 576) {
     if (mountains) {
-      mountains.style.height = "70vh";
-      mountains.style.top = "0";
+      mountains.style.height = '70vh';
+      mountains.style.top = '0';
     }
     if (mountainWrapper) {
-      mountainWrapper.style.height = "70vh";
+      mountainWrapper.style.height = '70vh';
     }
   }
   if (ratio < 0.7 && screenWidth > 576) {
     if (mountains) {
-      mountains.style.height = "60vh";
-      mountains.style.top = "0";
+      mountains.style.height = '60vh';
+      mountains.style.top = '0';
     }
     if (mountainWrapper && screenWidth > 576) {
-      mountainWrapper.style.height = "60vh";
+      mountainWrapper.style.height = '60vh';
     }
   }
   if (ratio < 0.5 && screenWidth < 576) {
     if (mountains) {
-      mountains.style.height = "47vh";
-      mountains.style.top = "0";
+      mountains.style.height = '47vh';
+      mountains.style.top = '0';
     }
     if (mountainWrapper && screenWidth < 576) {
-      mountainWrapper.style.height = "47vh";
-      mountainWrapper.style.minHeight = "0";
-      siteTitle.style.top = "38%";
-      arrow.style.display = "none";
-      scrollDown.style.display = "none";
-      mountainQuote.style.top = "47%"
-
+      mountainWrapper.style.height = '47vh';
+      mountainWrapper.style.minHeight = '0';
+      siteTitle.style.top = '38%';
+      arrow.style.display = 'none';
+      scrollDown.style.display = 'none';
+      mountainQuote.style.top = '47%';
     }
   }
 });
